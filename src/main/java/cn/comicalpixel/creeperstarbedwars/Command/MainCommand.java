386d99685bdd830e0d075c71a4bc6431368b5d3c
comicalpixel <133474289+comicalpixel@users.noreply.github.com>
2025-04-06 21:48:07 +0800

    修改了点东西，下周在继续写了，上学期间就优化和修点bug了
    修改了点东西，下周在继续写了，上学期间就优化和修点bug了

--- src/main/java/cn/comicalpixel/creeperstarbedwars/Command/MainCommand.java
+++ src/main/java/cn/comicalpixel/creeperstarbedwars/Command/MainCommand.java
@@ -1,6 +1,9 @@
 package cn.comicalpixel.creeperstarbedwars.Command;
 
+import cn.comicalpixel.creeperstarbedwars.Arena.Stats.GameStats;
+import cn.comicalpixel.creeperstarbedwars.Arena.Teams.GUI.TeamSel_GUI;
 import cn.comicalpixel.creeperstarbedwars.CreeperStarBedwars;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,11 +14,18 @@ public class MainCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String str, String[] args) {
 
         if (sender instanceof Player) {
+            Player p = (Player) sender;
             if (args.length >= 1) {
                 String arg1 = args[0];
                 arg1 = arg1.toLowerCase();
                 switch (arg1) {
-
+                    case "teamsel":
+                        if (GameStats.get() == 1) {
+                            TeamSel_GUI.open(p);
+                        } else {
+                            sender.sendMessage(ChatColor.RED + "The game has already started! You cannot do this now.");
+                        }
+                        break;
                 }
             } else {
                 sender.sendMessage("§aCreeper§eStar§fBedwars §7v" + CreeperStarBedwars.getInstance().getDescription().getVersion());
