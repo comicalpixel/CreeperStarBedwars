8f8cfdc91a8c46c98e0baa71bf9d328db3ec04b2
comicalpixel <133474289+comicalpixel@users.noreply.github.com>
2025-04-08 21:22:44 +0800

    修改多个部分，优化饱食度扣除限速

--- src/main/java/cn/comicalpixel/creeperstarbedwars/Command/MainCommand.java
+++ src/main/java/cn/comicalpixel/creeperstarbedwars/Command/MainCommand.java
@@ -1,8 +1,13 @@
 package cn.comicalpixel.creeperstarbedwars.Command;
 
+import cn.comicalpixel.creeperstarbedwars.Arena.GameData_cfg;
+import cn.comicalpixel.creeperstarbedwars.Arena.GamePlayers;
+import cn.comicalpixel.creeperstarbedwars.Arena.GameStart;
 import cn.comicalpixel.creeperstarbedwars.Arena.Stats.GameStats;
 import cn.comicalpixel.creeperstarbedwars.Arena.Teams.GUI.TeamSel_GUI;
+import cn.comicalpixel.creeperstarbedwars.Config.ConfigData;
 import cn.comicalpixel.creeperstarbedwars.CreeperStarBedwars;
+import cn.comicalpixel.creeperstarbedwars.Task.Game_Countdown_Task;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -24,6 +29,39 @@ public class MainCommand implements CommandExecutor {
                             TeamSel_GUI.open(p);
                         } else {
                             sender.sendMessage(ChatColor.RED + "The game has already started! You cannot do this now.");
+                        }
+                        break;
+                    case "start":
+                        // 检查游戏是否开始
+                        if (GameStats.get() == 1) {
+
+                            if (args.length == 1) {
+                                if (p.hasPermission("bedwars.start")) {
+                                    p.sendMessage(ConfigData.language_command_bwstart_done);
+                                    Game_Countdown_Task.countdown = 6;
+                                    Game_Countdown_Task.countdown_running = true;
+                                } else {
+                                    p.sendMessage(ConfigData.language_command_bwstart_nopermissions);
+                                }
+                            } else if (args.length == 2) {
+                                if (args[1].equalsIgnoreCase("debug")) {
+                                    if (p.isOp() || p.hasPermission("bedwars.admin")) {
+                                        GameStart.start();
+                                        Game_Countdown_Task.countdown_running = false;
+                                        Game_Countdown_Task.countdown = 0;
+                                    }
+                                }
+                            }
+
+                        } else {
+                            sender.sendMessage(ChatColor.RED + "The game has already started! You cannot do this now.");
+                        }
+                        break;
+                    case "setup":
+                        if (p.isOp() || p.hasPermission("bedwars.admin")) {
+                            sender.sendMessage("§7Please use the \"/setup\" command.");
+                        } else {
+                            p.sendMessage(ConfigData.language_command_nopermissions);
                         }
                         break;
                 }
