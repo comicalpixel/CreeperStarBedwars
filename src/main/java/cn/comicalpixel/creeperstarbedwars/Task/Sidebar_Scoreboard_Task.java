0f86f97ec100c5ee53455115ce84e70287052ea0
comicalpixel <133474289+comicalpixel@users.noreply.github.com>
2025-04-30 22:09:10 +0800

    等待大厅的计分板写完了，3天后再提交更新(外出用笔记本不方便及时提交更改)

--- src/main/java/cn/comicalpixel/creeperstarbedwars/Task/Sidebar_Scoreboard_Task.java
+++ src/main/java/cn/comicalpixel/creeperstarbedwars/Task/Sidebar_Scoreboard_Task.java
@@ -2,7 +2,6 @@ package cn.comicalpixel.creeperstarbedwars.Task;
 
 import cn.comicalpixel.creeperstarbedwars.Arena.GamePlayers;
 import cn.comicalpixel.creeperstarbedwars.Arena.Stats.GameStats;
-import cn.comicalpixel.creeperstarbedwars.Arena.Teams.TeamManager;
 import cn.comicalpixel.creeperstarbedwars.Config.ConfigData;
 import cn.comicalpixel.creeperstarbedwars.CreeperStarBedwars;
 import cn.comicalpixel.creeperstarbedwars.Listener.BwimResItemManager;
@@ -32,7 +31,7 @@ public class Sidebar_Scoreboard_Task {
 
                 if (GameStats.get() == 1) {
                     for (Player p : GamePlayers.players) {
-                        send_1(p);
+                        send_lobby(p);
                     }
                 }
 
@@ -41,7 +40,7 @@ public class Sidebar_Scoreboard_Task {
 
     }
 
-    public void send_1(Player p) {
+    public void send_lobby(Player p) {
         Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
         Objective objective = null;
         List<String> message_list = new ArrayList<>();
@@ -55,6 +54,7 @@ public class Sidebar_Scoreboard_Task {
             objective = scoreboard.registerNewObjective("Error Lobby Sidebar Scoreboard", "dummy");
         }
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+
         int slot = 0;
         message_list.remove(0);
         for (String s : message_list) {
@@ -70,5 +70,36 @@ public class Sidebar_Scoreboard_Task {
         p.setScoreboard(scoreboard);
 
     }
+
+
+    public void send_InGame(Player p) {
+        Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        Objective objective = null;
+        List<String> message_list = new ArrayList<>();
+        if (GamePlayers.players.contains(p)) {
+            message_list.addAll(ConfigData.language_sidebarboard_list_playing_player);
+            objective = scoreboard.registerNewObjective(ConfigData.language_sidebarboard_list_playing_player.get(0)+" ", "dummy");
+        } else if (GamePlayers.specs.contains(p)) {
+            message_list.addAll(ConfigData.language_sidebarboard_list_playing_spec);
+            objective = scoreboard.registerNewObjective(ConfigData.language_sidebarboard_list_playing_spec.get(0)+" ", "dummy");
+        } else {
+            objective = scoreboard.registerNewObjective("Error Game Sidebar Scoreboard", "dummy");
+        }
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+
+        int slot = 0;
+        message_list.remove(0);
+        for (String s : message_list) {
+            s = s.replace("{date}", MessageVariableUtils.getDate());
+            s = MessageVariableUtils.gameMapInformation_p_s(s);
+            s = s.replace("{bwim}", BwimResItemManager.Companion.getModeName(p));
+            Score score = objective.getScore(s);
+            score.setScore(message_list.size() - slot);
+            slot++;
+        }
+
+        p.setScoreboard(scoreboard);
+    }
+
 
 }
