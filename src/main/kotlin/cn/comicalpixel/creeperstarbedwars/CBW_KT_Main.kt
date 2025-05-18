33552f78f6f77545715787ab762902df47e2692d
comicalpixel <133474289+comicalpixel@users.noreply.github.com>
2025-05-18 19:30:50 +0800

    缓更几天，我想留多点时间和朋友玩bmc2，然后准备好下周的竞赛的初赛，感谢你的支持与理解

--- src/main/kotlin/cn/comicalpixel/creeperstarbedwars/CBW_KT_Main.kt
+++ src/main/kotlin/cn/comicalpixel/creeperstarbedwars/CBW_KT_Main.kt
@@ -3,8 +3,7 @@ package cn.comicalpixel.creeperstarbedwars
 import cn.comicalpixel.creeperstarbedwars.Listener.ServerMotdListener
 import org.bukkit.Bukkit
 
-// 别问为啥我用kt (
-// 无聊才用的
+// 别问为啥我用kt (qwq
 // @Author Xiaol789zxc
 
 class CBW_KT_Main {
